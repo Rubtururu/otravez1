927dff7ec7bd0d7f89f7cd6a46872416b21b0d29
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2025-02-23 23:39:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,100 +1,60 @@
-// app.js
-let web3;
-let contract;
-let account;
-const CONTRACT_ADDRESS = '0xcB02cE74AeA8aC527d8952Fb0393965fdD3229c5'; // Dirección del contrato desplegado
-const CONTRACT_ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"PRICE","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burnWeed","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"buyUnits","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"claimRewards","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"distributeDividends","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getGlobalStats","outputs":[{"internalType":"uint256[8]","name":"contractData","type":"uint256[8]"},{"internalType":"uint256","name":"currentBalance","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_user","type":"address"}],"name":"getUserStats","outputs":[{"internalType":"uint256[6]","name":"userData","type":"uint256[6]"},{"internalType":"uint256[2]","name":"pendingRewards","type":"uint256[2]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"stats","outputs":[{"internalType":"uint256","name":"totalInvested","type":"uint256"},{"internalType":"uint256","name":"totalWeedProduced","type":"uint256"},{"internalType":"uint256","name":"totalBurned","type":"uint256"},{"internalType":"uint256","name":"totalDividends","type":"uint256"},{"internalType":"uint256","name":"totalBurnRewards","type":"uint256"},{"internalType":"uint256","name":"lastDistribution","type":"uint256"},{"internalType":"uint256","name":"activeUsers","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"users","outputs":[{"internalType":"uint256","name":"invested","type":"uint256"},{"internalType":"uint256","name":"weedProduced","type":"uint256"},{"internalType":"uint256","name":"burned","type":"uint256"},{"internalType":"uint256","name":"dividendsReceived","type":"uint256"},{"internalType":"uint256","name":"burnRewards","type":"uint256"},{"internalType":"uint256","name":"lastUpdate","type":"uint256"}],"stateMutability":"view","type":"function"}]; // ABI del contrato
+const contractAddress = "0x4AD199a7c1ED96636eb0F7F33475F2f3a4461203";
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"claimRewards","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"contributeToGamePool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"depositBoxBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"depositToBox","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"distributeRewards","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"earnGoo","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"gooBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastDistributionTime","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalDepositBox","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalGamePool","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
 
-window.addEventListener('load', async () => {
-    // Conectar a MetaMask
-    if(window.ethereum) {
-        web3 = new Web3(window.ethereum);
-        try {
-            await window.ethereum.enable();
-            account = (await web3.eth.getAccounts())[0];
-            initContract();
-            checkAdmin();
-            setInterval(updateStats, 5000);
-        } catch(error) {
-            alert("Error al conectar la wallet");
-        }
-    }
+let provider, signer, contract;
+
+document.addEventListener('DOMContentLoaded', () => {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
+    contract = new ethers.Contract(contractAddress, abi, signer);
+
+    updateBalances();
+    updateStats();
 });
 
-function initContract() {
-    contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
-    updateStats();
+async function updateBalances() {
+    const user = await contract.users(await signer.getAddress());
+    document.getElementById('gooAmount').innerText = user.gooBalance.toString();
+
+    const depositBoxTotal = await contract.totalDepositBox();
+    document.getElementById('depositBoxTotal').innerText = depositBoxTotal.toString();
+
+    const gamePoolTotal = await contract.totalGamePool();
+    document.getElementById('gamePoolTotal').innerText = gamePoolTotal.toString();
 }
 
 async function updateStats() {
-    // Estadísticas globales
-    const globalStats = await contract.methods.getGlobalStats().call();
-    document.getElementById('totalInvested').innerText = web3.utils.fromWei(globalStats[0], 'ether');
-    document.getElementById('totalWeedProduced').innerText = web3.utils.fromWei(globalStats[1], 'ether');
-    document.getElementById('totalBurned').innerText = web3.utils.fromWei(globalStats[2], 'ether');
-    document.getElementById('lastDistribution').innerText = new Date(globalStats[5] * 1000).toLocaleString();
-    
-    // Estadísticas usuario
-    const userStats = await contract.methods.getUserStats(account).call();
-    document.getElementById('userInvested').innerText = web3.utils.fromWei(userStats[0], 'ether');
-    document.getElementById('userWeedProduced').innerText = web3.utils.fromWei(userStats[1], 'ether');
-    document.getElementById('userBurned').innerText = web3.utils.fromWei(userStats[2], 'ether');
-    document.getElementById('pendingRewards').innerText = 
-        `${parseFloat(web3.utils.fromWei(userStats[3], 'ether')).toFixed(6)} BNB`;
+    const [depositBoxReward, gamePoolReward] = await contract.getDailyRewards();
+    document.getElementById('dailyDepositBoxReward').innerText = depositBoxReward.toString();
+    document.getElementById('dailyGamePoolReward').innerText = gamePoolReward.toString();
+
+    const userShare = await contract.getUserShare(await signer.getAddress());
+    document.getElementById('userShare').innerText = userShare.toString();
+
+    const userRank = await contract.getUserRank(await signer.getAddress());
+    document.getElementById('userRank').innerText = userRank.toString();
 }
 
-async function buyUnits() {
-    const bnbAmount = document.getElementById('bnbAmount').value;
-    const weiAmount = web3.utils.toWei(bnbAmount, 'ether');
-    
-    try {
-        await contract.methods.buyUnits().send({
-            from: account,
-            value: weiAmount
-        });
-        updateStats();
-    } catch(error) {
-        alert("Error en la compra: " + error.message);
-    }
-}
+document.getElementById('investBNBButton').addEventListener('click', async () => {
+    const amount = ethers.utils.parseEther("0.0001"); // Example amount
+    await contract.investBNB({ value: amount });
+    await updateBalances();
+});
 
-async function burnWeed() {
-    const amount = document.getElementById('weedToBurn').value;
-    
-    try {
-        await contract.methods.burnWeed(amount).send({ from: account });
-        updateStats();
-    } catch(error) {
-        alert("Error al quemar: " + error.message);
-    }
-}
+document.getElementById('depositBoxButton').addEventListener('click', async () => {
+    const amount = document.getElementById('depositBoxAmount').value;
+    await contract.depositToBox(amount);
+    await updateBalances();
+});
 
-async function claimRewards() {
-    try {
-        await contract.methods.claimRewards().send({ from: account });
-        updateStats();
-    } catch(error) {
-        alert("Error al reclamar: " + error.message);
-    }
-}
+document.getElementById('gamePoolButton').addEventListener('click', async () => {
+    const amount = document.getElementById('gamePoolAmount').value;
+    await contract.contributeToGamePool(amount);
+    await updateBalances();
+});
 
-async function distributeDividends() {
-    try {
-        await contract.methods.distributeDividends().send({ from: account });
-        updateStats();
-    } catch(error) {
-        alert("Error en distribución: " + error.message);
-    }
-}
-
-async function checkAdmin() {
-    const owner = await contract.methods.owner().call();
-    if(account.toLowerCase() === owner.toLowerCase()) {
-        document.getElementById('adminSection').style.display = 'block';
-    }
-}
-
-// Helpers
-function formatWei(value) {
-    return parseFloat(web3.utils.fromWei(value, 'ether')).toFixed(6);
-}
+document.getElementById('claimRewardsButton').addEventListener('click', async () => {
+    await contract.claimRewards();
+    await updateBalances();
+    await updateStats();
+});
